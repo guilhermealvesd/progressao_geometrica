558f45f320c53a98aaa2139424fea1ab75c810c1
guilhermealvesd <alvessig@gmail.com>
2024-06-05 21:21:57 -0300

    Versão 1.0.

---
+++ main.py
@@ -0,0 +1,11 @@
+#Função para calcular a progressão geométrica
+
+calcular_pg = lambda x: x * 2
+
+#Lista Numérica
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#Mostra o resultado da progressão geométrica
+
+print(list(map(calcular_pg, numeros)))
